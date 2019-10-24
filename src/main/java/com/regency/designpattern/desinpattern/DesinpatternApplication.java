25eb6e06de3539bdf1414fff6f38c3275750c0a6
pratyusb <pratyus.j2ee@gmail.com>
2019-10-24 22:50:05 +0530

    Initial commit

---
+++ src/main/java/com/regency/designpattern/desinpattern/DesinpatternApplication.java
@@ -0,0 +1,13 @@
+package com.regency.designpattern.desinpattern;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class DesinpatternApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(DesinpatternApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/regency/designpattern/desinpattern/DesinpatternApplicationTests.java
@@ -0,0 +1,13 @@
+package com.regency.designpattern.desinpattern;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class DesinpatternApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
